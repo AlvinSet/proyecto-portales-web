3cbb28c4338ba9210b3292804f24b359a03359eb
AlvinSet <alvingm17@gmail.com>
2024-04-22 11:37:34 -0300

    Create Project with Vite-Vue

---
+++ zetaproject/src/App.vue
@@ -0,0 +1,23 @@
+<script>
+export default{
+  name:'App',
+}
+</script>
+
+<template>
+<nav class="bg-slate-700 text-gray-50 p-4 flex justify-between items-center">
+  <p class="text-2xl"><a href="">Zeta's Shit</a></p>
+  <ul class="flex gap-4">
+    <li><a href="#">Home</a></li>
+    <li><a href="#">Chat Publico</a></li>
+    <li><a href="#">Iiniciar Sesion</a></li>
+    <li><a href="#">Registrarse</a></li>
+  </ul>
+</nav>
+<main>
+La madafokin Grasa. 
+</main>
+<footer class=" bg-slate-900 text-gray-50 flex justify-center h-[100px] items-center">
+<p>Zeta Company All Rights Reserved </p>
+</footer>
+</template>
